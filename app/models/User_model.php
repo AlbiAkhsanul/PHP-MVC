170fa1ece9db29ce6f8ec61affaa7d45bf318295
Albi Akhsanul <albiakhsanulhakim24@gmail.com>
2023-04-09 13:34:48 +0700

    initial commit

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model {
+    private $name = 'Albi';
+
+    public function getUser(){
+        return $this->name;
+    }
+}
---
+++ app/views/about/page.php
@@ -0,0 +1,3 @@
+<div class="container">
+    <h1>Selamat Datang Di About/Page</h1>
+</div>
